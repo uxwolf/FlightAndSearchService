b96c6965facbc13f96ed037c39efe238160c4ae5
uxwolf <135868251+uxwolf@users.noreply.github.com>
2025-04-12 13:37:32 +0530

    BodyParser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async ()=>{
+
     //create express object
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
     app.listen(PORT , ()=>{
         console.log(`server started at port: ${PORT}`);
     });
